7436223da060c286101ef25e8c26f833e3a95bae
Brad Edmonds <bcedmonds0417@gmail.com>
2021-08-12 20:55:34 -0500

    make axios request to github api, found forks, watchers, and stars counts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,13 +8,21 @@
 </style>
 
 <script>
+const axios = require("axios");
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
     };
   },
-  created: function () {},
+  created: function () {
+    axios.get("https://api.github.com/repos/vuejs/vue").then(function (response) {
+      console.log(response.data);
+      console.log(response.data.forks);
+      console.log(response.data.watchers);
+      console.log(response.data.stargazers_count);
+    });
+  },
   methods: {},
 };
 </script>
